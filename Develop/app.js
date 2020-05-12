15d2fbda48aab7f6b62ab1ecb72603c7336a707f
Michael P Nance <michael.p.nance@gmail.com>
2020-05-12 00:14:49 -0400

    Added input validation. Updated user prompts to make them or terse.

--- Develop/app.js
+++ Develop/app.js
@@ -6,47 +6,20 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const util = require("util");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
-
 // Asks the user if they would like to add an employee and returns correspoding boolean value
 async function addEmployee() {
     const response = await inquirer.prompt({
         message: "Add a(nother) employee?",
         name: "continue",
-        type: "list",
-        choices: ["yes", "no"],
+        type: "confirm",
     }, );
-    return response.continue === "yes";
+    return response.continue;
 }
 
 // Asks the user to provide role for new employee and returns corresponding choice as string
@@ -64,22 +37,33 @@ async function getRole() {
 // an object e.g. {name: "bob", id: 1, email: "bob@gmail.com"}
 async function getCommonInfo() {
     return await inquirer.prompt([{
-        message: "Enter employee's name...",
+        message: "Enter employee's name",
         name: "name",
     }, {
-        message: "ID number...",
+        message: "ID number",
         name: "id",
+        validate: id => {
+            return /\d/.test(id);
+        }
     }, {
-        message: "email address...",
+        message: "email address",
         name: "email",
+        validate: email => {
+            // I found this regex on the internet. We'll just assume it works.
+            return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+        },
     }]);
 }
 
 // Asks user to provide new employee's GitHub account name and returns response as string
 async function getEngineerInfo() {
     const response = await inquirer.prompt({
-        message: "and GitHub account name.",
+        message: "GitHub account.",
         name: "github",
+        validate: github => {
+            // Ditto
+            return /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i.test(github);
+        }
     });
     return response.github;
 }
@@ -87,7 +71,7 @@ async function getEngineerInfo() {
 // Asks user to proivde new employee's School name and returns response as string
 async function getInternInfo() {
     const response = await inquirer.prompt({
-        message: "and school name.",
+        message: "school name.",
         name: "school",
     });
     return response.school;
@@ -96,8 +80,11 @@ async function getInternInfo() {
 // Asks user to provide new employee's office number and returns response as string
 async function getManagerInfo() {
     const response = await inquirer.prompt({
-        message: "and office number",
+        message: "office number",
         name: "officeNumber",
+        validate: officeNumber => {
+            return /\d/.test(officeNumber);
+        },
     });
     return response.officeNumber;
 }
